89f70c5b8e6f1da5c73eba80e123cdac92bf8088
Alina Garipova <alina.garipova@headmade.pro>
2019-08-14 11:33:05 +0300

    Updates

---
+++ responsive-8e89bd4f99abc18f75ce.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[8],{85:function(n,w,o){}},[[85,0]]]);
+//# sourceMappingURL=responsive-8e89bd4f99abc18f75ce.js.map
---
+++ style-ff00abb57388b97157be.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[9],{84:function(n,w,o){}},[[84,0]]]);
+//# sourceMappingURL=style-ff00abb57388b97157be.js.map
